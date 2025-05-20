2ea8f287e61623e55dae501fad08cab0cb021b9c
belan <smakki.92@gmail.com>
2025-05-20 12:36:24 +0300

    First commit

---
+++ ProHomeWork3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+
+namespace ProHomeWork3;
+
+internal static class Program
+{
+    private static async Task Main()
+    {
+        await CountSpacesInThreeFiles();
+        Console.WriteLine("");
+        await CountSpacesInAllFilesInDirectory("D:\\source\\projects\\learning\\ProHomeWork3\\ProHomeWork3");
+        Console.WriteLine("Press any key to continue...");
+    }
+
+    private static async Task CountSpacesInAllFilesInDirectory(string directory)
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        var files = Directory.GetFiles(directory);
+        var tasks = new Dictionary<string, Task<int>>();
+        
+        foreach (var fileInfo in files)
+        {
+            var task = Task.Run(() => CountSpacesInFile(fileInfo));
+            tasks.Add(fileInfo, task);
+        }
+        foreach (var keyValuePair in tasks)
+        {
+            try
+            {
+                var result = await keyValuePair.Value;
+                Console.WriteLine($"In file '{keyValuePair.Key}': {result} spaces");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error reading file {keyValuePair.Key}: {e.Message}");
+                throw;
+            }
+        }
+        sw.Stop();
+        Console.WriteLine($"Calculating spaces in all files in directory completed in {sw.ElapsedMilliseconds} milliseconds");
+
+    }
+    private static async Task CountSpacesInThreeFiles()
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        var files = new string[]
+        { 
+            "NewFile1.txt",
+            "NewFile2.txt",
+            "NewFile3.txt"
+        };
+        var tasks = new Dictionary<string, Task<int>>();
+        
+        foreach (var fileInfo in files)
+        {
+            var task = Task.Run(() => CountSpacesInFile(fileInfo));
+            tasks.Add(fileInfo, task);
+        }
+        foreach (var keyValuePair in tasks)
+        {
+            try
+            {
+                var result = await keyValuePair.Value;
+                Console.WriteLine($"In file '{keyValuePair.Key}': {result} spaces");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error reading file {keyValuePair.Key}: {e.Message}");
+                throw;
+            }
+        }
+        sw.Stop();
+        Console.WriteLine($"Calculating spaces in three files completed in {sw.ElapsedMilliseconds} milliseconds");
+    }
+
+    private static int CountSpacesInFile(string file)
+    {
+        var text = File.ReadAllText(file);
+        return text.Count((c) => c == ' ');
+    }
+}
+
+
+
